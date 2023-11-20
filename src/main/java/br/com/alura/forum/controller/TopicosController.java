771c485ec3e78effec542760b9f73173ddd02b10
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-20 17:23:37 -0300

    refatorando classe topicoscontroller

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,33 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping(value = "/topicos")
 public class TopicosController {
     
-    @RequestMapping("/topicos")
-    @ResponseBody
-    public List<TopicoDto> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring","Programação"));
-        return TopicoDto.converter( Arrays.asList(topico, topico, topico));
-    }
+    @Autowired
+    private TopicoRepository topicoRepository;
 
+    @GetMapping
+    public List<TopicoDto> lista(String nomeCurso){
+        System.out.println(nomeCurso);
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }
+        else{
+             List<Topico> topicos = (List<Topico>) topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+    }
 }
