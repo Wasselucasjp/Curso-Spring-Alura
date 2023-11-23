532ef1148247777a86ac1e090c9ee165289b7752
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-23 17:15:52 -0300

    tirando importacao inutilizavel

--- src/main/java/br/com/alura/forum/config/validation/ErroDeValidacaoHandler.java
+++ src/main/java/br/com/alura/forum/config/validation/ErroDeValidacaoHandler.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.alura.forum.controller.dto.ErrorDeFormularioDTO;
-
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
     /**
