c94dc6f8c14b774e5a5fde572e058b61e005a91b
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-23 12:51:36 -0300

    alteracao no pacote

--- src/main/java/br/com/alura/forum/config/validation/ErroDeValidacaoHandler.java
+++ src/main/java/br/com/alura/forum/config/validation/ErroDeValidacaoHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.alura.forum.dto.ErrorDeFormularioDTO;
+import br.com.alura.forum.controller.dto.ErrorDeFormularioDTO;
 
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
